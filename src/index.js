7cc2eb93727e41756a773bbf86d21129257d2d18
Abdelrahman Ali <aezzat@onica.com>
2022-01-18 08:12:01 +0200

    added server name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://apollo-test-liftoff5.herokuapp.com',
+  uri: 'https://apollo-test-liftoff5-server.herokuapp.com',
   cache: new InMemoryCache(),
 });
 
